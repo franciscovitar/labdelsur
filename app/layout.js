b8e419a51dd2a4c8036a3f245c3c97ff45f1cc20
francisco <franvitar15@gmail.com>
2024-05-22 15:22:27 -0300

    fix

--- app/layout.js
+++ app/layout.js
@@ -6,14 +6,32 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const inter = Jost({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "LabDelSur",
-  description: "",
+  title: "LabDelSur | Análisis Clínicos y Microbiológicos de Calidad en Lanús",
+  description:
+    "LabDelSur ofrece análisis clínicos y microbiológicos de baja, mediana y alta complejidad en Lanús. Servicios de hisopados, extracciones a domicilio, resultados online y atención a empresas e instituciones.",
+  keywords: [
+    "análisis clínicos",
+    "análisis microbiológicos",
+    "hisopados COVID-19",
+    "extracciones a domicilio",
+    "resultados online",
+    "laboratorio Lanús",
+    "exámenes pre ocupacionales",
+    "servicios de salud",
+    "calidad en análisis clínicos",
+    "laboratorio microbiología",
+    "atención personalizada",
+    "test de antígenos",
+    "cobertura obras sociales",
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body translate="no" className={inter.className}>
+        {children}
+      </body>
     </html>
   );
 }
